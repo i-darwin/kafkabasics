ffbe84dfd410c326e1c1d9e109b48122ae0876ef
i-darwin <49554397+i-darwin@users.noreply.github.com>
2021-01-28 11:55:21 +0700

    Update KafkaProducerPlainExample.java
    Fix Class Name so it follows package name

--- src/com/example/kafka/producer/KafkaProducerPlainExample.java
+++ src/com/example/kafka/producer/KafkaProducerPlainExample.java
@@ -8,7 +8,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-public class KafkaConnectProduce {
+public class KafkaProducerPlainExample {
 
 	public static void main(String[] args) {
 		//String bootstrapServer = "10.1.77.104:9092";
