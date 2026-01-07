e107e89af23000a34ad730a5a9d4f9e040db4a95
diana <diana.lly19@gmail.com>
2026-01-07 22:00:16 +0800

    change text diana

--- src/com/example/MainClass.java
+++ src/com/example/MainClass.java
@@ -3,6 +3,6 @@ package com.example;
 public class MainClass {
 
     public static void main(String[] args) {
-        System.out.println("test indra");
+        System.out.println("test diana");
     }
 }
